649f287fb3d49adc816d03dc7c20274f178d1fa0
josue velasquez <josue.velasquez@chappsa.com>
2023-11-13 16:31:36 -0600

    Pruebas en app mantenimiento

---
+++ mantenimiento/mantenimiento/doctype/empleado/test_empleado.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, CHAPPSA and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestEmpleado(FrappeTestCase):
+	pass
---
+++ mantenimiento/mantenimiento/doctype/proceso/proceso.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, CHAPPSA and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Proceso', {
+	// refresh: function(frm) {
+
+	// }
+});
